61b24d4def50de3e6eaa419545fbf884175b96d5
Shovon588 <shovon@cuttingroom>
2022-07-21 00:42:09 +0600

    #2 Starter action and reducer config

---
+++ client/src/redux/actions/userActionTypes.ts
@@ -0,0 +1,19 @@
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_START = 'LOGIN_START';
+export const LOGIN_END = 'LOGIN_END';
+
+interface LoginStart {
+  type: typeof LOGIN_START;
+}
+
+interface LoginSuccess {
+  type: typeof LOGIN_SUCCESS;
+  username: string;
+  token: string;
+}
+
+interface LoginEnd {
+  type: typeof LOGIN_END;
+}
+
+export type UserDispatchType = LoginSuccess | LoginStart | LoginEnd;
